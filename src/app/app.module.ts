bbdfbe26618110714f339bef2cfae677d48e3b82
LuisZamMX <luis.felipe.zamora@comunidad.unam.mx>
2020-07-29 03:02:24 -0500

    Se configuró el archivo app.module.ts para importar los módulos correspondientes para el servicio ItemService y para configurar las rutas de frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//Se importan los Módulos RouterModule y Routes para hacer el enrutamiento del front end
+import { RouterModule, Routes } from '@angular/router'
+//Se importa el módulo HttpCLientModule para hacer peticiones a la api (backend)
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+//Se importan los componentes creados
 import { HeaderComponent } from './components/header/header.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ListaItemsComponent } from './components/lista-items/lista-items.component';
 import { GraficaItemsComponent } from './components/grafica-items/grafica-items.component';
+
+//Se crea la constante rutas para manejar las rutas de los componentes de frontend
+const rutas: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  },
+  {
+    path: 'graficas',
+    component: GraficaItemsComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +33,13 @@ import { GraficaItemsComponent } from './components/grafica-items/grafica-items.
     GraficaItemsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(rutas, {
+      enableTracing: true,
+      paramsInheritanceStrategy: "always",
+      useHash: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
