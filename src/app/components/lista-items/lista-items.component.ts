35ba82626b9b17aa3a6346c0efb02556a8fb2dce
LuisZamMX <luis.felipe.zamora@comunidad.unam.mx>
2020-07-29 03:06:00 -0500

    Se configura el componente listaItemsComponent para que le provea sus servicios ItemServicey se configura el layout para que muestre los datos de los items

--- src/app/components/lista-items/lista-items.component.ts
+++ src/app/components/lista-items/lista-items.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+//Se importa el Servicio ItemService 
+import { ItemService } from '../../services/item.service'
+
+//Se importa el modelo Item para poder guardar los datos en un arreglo Item[]
+import { Item } from '../../models/item'
 
 @Component({
   selector: 'app-lista-items',
   templateUrl: './lista-items.component.html',
-  styleUrls: ['./lista-items.component.css']
+  styleUrls: ['./lista-items.component.css'],
+  //Se agrega el servicio ItemService para realizar las peticiones correspondientes a la api
+  providers: [ItemService] 
 })
 export class ListaItemsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public itemService: ItemService) { 
+  }
 
   ngOnInit(): void {
+    //Se ejecutan los métodos al iniciar el componente
+    this.getItems();
+    this.getTotal();
+  }
+
+  //Se llama al método getItems del servicio ItemService para obtener la lista de items
+  getItems(){
+    this.itemService.getItems()
+    .subscribe((res: any) => {
+      this.itemService.items = res as Item[];
+      console.log(res)
+    })
+  }
+  //Se llama al método getItems del servicio ItemService para obtener el total de cantidades de los items
+  getTotal(){
+    this.itemService.getItemsTotal()
+    .subscribe((res: any) => {
+      this.itemService.total = res;
+    })
   }
 
 }
